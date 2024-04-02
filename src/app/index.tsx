a340a8696d6666a3abf5319d35cc5c0fcba70bd6
PedroMakengo <pedromakengomiguel@gmail.com>
2024-04-02 14:33:10 +0100

    Estrutura inicial do projeto, instalação das dependencias

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,22 +1,9 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text } from 'react-native'
 
 export default function Home() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello React Native!</Text>
+    <View>
+      <Text>Hello React Native!</Text>
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'red',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    color: 'white',
-    fontSize: 20,
-  },
-})
