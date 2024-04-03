b0508494e8787d4b257b592ca3500a21893cf4cf
PedroMakengo <pedromakengomiguel@gmail.com>
2024-04-03 14:15:06 +0100

    Atualizações das configurações do meu app no android

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,8 @@ import { View, Text } from 'react-native'
 
 export default function Home() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text>Hello React Native</Text>
+    <View className="flex-1 justify-center items-center bg-green-500">
+      <Text className="text-white">Hello React Native</Text>
     </View>
   )
 }
